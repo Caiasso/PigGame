aba650e97470d7b00a6d4aa311cd5ae5ee134536
Caiasso <guilhermecaiasso@gmail.com>
2024-07-15 18:26:22 -0300

    Finished game logic for all functionalities

--- script.js
+++ script.js
@@ -17,49 +17,83 @@ score1El.textContent = 0;
 diceEl.classList.add(`hidden`);
 
 const scores = [0, 0];
+const limitScore = 20;
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const init = function () {
+  player0El.classList.remove(`player--winner`);
+  player1El.classList.remove(`player--winner`);
+  player1El.classList.remove(`player--active`);
+  player0El.classList.add(`player--active`);
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add(`hidden`);
+
+  scores[0] = 0;
+  scores[1] = 0;
+
+  currentScore = 0;
+
+  activePlayer = 0;
+
+  playing = true;
+};
 
 const switchPlayers = function () {
-  document.body.style.backgroundImage =
-    activePlayer === 1
-      ? `linear-gradient(to top left, #753682 0%, #bf2e34 100%)`
-      : `linear-gradient(to bottom right, #753682 0%, #bf2e34 100%)`;
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle(`player--active`);
-  player1El.classList.toggle(`player--active`);
+  if (playing) {
+    document.body.style.backgroundImage =
+      activePlayer === 1
+        ? `linear-gradient(to top left, #753682 0%, #bf2e34 100%)`
+        : `linear-gradient(to bottom right, #753682 0%, #bf2e34 100%)`;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle(`player--active`);
+    player1El.classList.toggle(`player--active`);
+  }
 };
 
 btnRoll.addEventListener(`click`, function () {
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  diceEl.classList.remove(`hidden`);
-  diceEl.src = `dice-${dice}.png`;
+    diceEl.classList.remove(`hidden`);
+    diceEl.src = `dice-${dice}.png`;
 
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    switchPlayers();
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayers();
+    }
   }
 });
 
 btnHold.addEventListener(`click`, function () {
-  scores[activePlayer] += currentScore;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
 
-  if (scores[activePlayer] >= 100) {
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add(`player--winner`);
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove(`player--active`);
-  } else {
-    switchPlayers();
+    if (scores[activePlayer] >= limitScore) {
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add(`player--winner`);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove(`player--active`);
+      diceEl.classList.add(`hidden`);
+      playing = false;
+    } else {
+      switchPlayers();
+    }
   }
 });
+
+btnNew.addEventListener(`click`, init);
