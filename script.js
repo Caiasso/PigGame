fa25ce611a31f5bb0855a4992dd77e3d288a9cc5
Caiasso <guilhermecaiasso@gmail.com>
2024-07-14 21:35:06 -0300

    Oh yes, the flippin score required to win is >= 100

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ btnHold.addEventListener(`click`, function () {
   document.getElementById(`score--${activePlayer}`).textContent =
     scores[activePlayer];
 
-  if (scores[activePlayer] > 100) {
+  if (scores[activePlayer] >= 100) {
     document
       .querySelector(`.player--${activePlayer}`)
       .classList.add(`player--winner`);
