ffedda585710081b61c9753dcbc00e2c1cfbfbd0
Caiasso <guilhermecaiasso@gmail.com>
2024-07-14 21:30:59 -0300

    Implemented game logic and some visual stuff

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player0El = document.querySelector(`.player--0`);
+const player1El = document.querySelector(`.player--1`);
 const score0El = document.getElementById(`score--0`);
 const score1El = document.getElementById(`score--1`);
 const current0El = document.getElementById(`current--0`);
@@ -14,10 +16,50 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add(`hidden`);
 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
-btnRoll.addEventListener(`click`, function() {
-    const dice = Math.trunc(Math.random() * 6) + 1;
+const switchPlayers = function () {
+  document.body.style.backgroundImage =
+    activePlayer === 1
+      ? `linear-gradient(to top left, #753682 0%, #bf2e34 100%)`
+      : `linear-gradient(to bottom right, #753682 0%, #bf2e34 100%)`;
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle(`player--active`);
+  player1El.classList.toggle(`player--active`);
+};
 
-    diceEl.classList.remove(`hidden`);
-    diceEl.src = `dice-${dice}.png`;
-})
+btnRoll.addEventListener(`click`, function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  diceEl.classList.remove(`hidden`);
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    switchPlayers();
+  }
+});
+
+btnHold.addEventListener(`click`, function () {
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  if (scores[activePlayer] > 100) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add(`player--winner`);
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove(`player--active`);
+  } else {
+    switchPlayers();
+  }
+});
